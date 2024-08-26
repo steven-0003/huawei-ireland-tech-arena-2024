2845fcfa36c1766b6a3c5def760ecfccc6456823
Steven <stevenmoussa03@gmail.com>
2024-08-26 15:02:24 +0100

    Fix datacenter tests

--- tests/datacenter_test.py
+++ tests/datacenter_test.py
@@ -16,24 +16,24 @@ class datacenterTest(unittest.TestCase):
     def test_buyServer(self):
         self.datacenter.buy_server('CPU.S1_low', 'server-1', 1)
         self.assertEqual(self.datacenter.inventory_level, 2)
-        self.assertEqual(self.datacenter.inventory['CPU.S1_low'], [[1,'server-1']])
+        self.assertEqual(self.datacenter.inventory['CPU.S1_low'], [(1,'server-1')])
 
     def test_sellServer(self):
-        self.datacenter.inventory['CPU.S1_low'].append([1,'server-1'])
+        self.datacenter.inventory['CPU.S1_low'].append((1,'server-1'))
         self.datacenter.inventory_level += 2
         self.datacenter.sell_server('CPU.S1_low')
         self.assertEqual(self.datacenter.inventory_level, 0)
         self.assertEqual(self.datacenter.inventory['CPU.S1_low'], [])
 
     def test_checkLifetime(self):
-        self.datacenter.inventory['CPU.S1_low'].append([1,'server-1'])
+        self.datacenter.inventory['CPU.S1_low'].append((1,'server-1'))
         self.datacenter.inventory_level += 2
         self.datacenter.check_lifetime(1)
-        self.assertEqual(self.datacenter.inventory['CPU.S1_low'], [[1,'server-1']])
+        self.assertEqual(self.datacenter.inventory['CPU.S1_low'], [(1,'server-1')])
         self.assertEqual(self.datacenter.inventory_level, 2)
 
     def test_checkLifetimeRemove(self):
-        self.datacenter.inventory['CPU.S1_low'].append([1,'server-1'])
+        self.datacenter.inventory['CPU.S1_low'].append((1,'server-1'))
         self.datacenter.inventory_level += 2
         self.datacenter.check_lifetime(98)
         self.assertEqual(self.datacenter.inventory_level, 0)
@@ -47,5 +47,5 @@ class datacenterTest(unittest.TestCase):
         self.datacenter.buy_server('GPU.S1_low', 'server-5', 1)
         self.datacenter.buy_server('GPU.S1_low', 'server-6', 1)
         self.assertEqual(self.datacenter.inventory_level, 18)
-        self.assertEqual(self.datacenter.inventory['CPU.S1_low'], [[1,'server-1'],[1,'server-2'],[1,'server-3']])
-        self.assertEqual(self.datacenter.inventory['GPU.S1_low'], [[1,'server-4'],[1,'server-5'],[1,'server-6']])
+        self.assertEqual(self.datacenter.inventory['CPU.S1_low'], [(1,'server-1'),(1,'server-2'),(1,'server-3')])
+        self.assertEqual(self.datacenter.inventory['GPU.S1_low'], [(1,'server-4'),(1,'server-5'),(1,'server-6')])
